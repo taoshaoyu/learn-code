5e01e53afc8283cdec5ad4da1d683c533d0dedef
taosy <shaoyu.tao@intel.com>
2023-01-14 00:16:51 -0500

    add files

---
+++ py/basic-topic/decorator/s1.py
@@ -0,0 +1,18 @@
+# 基本装饰器
+
+def hello(fn):
+    def wrapper1():
+        print("hello, %s" % fn.__name__)
+        fn()
+        print("goodby, %s" % fn.__name__)
+    return wrapper1
+
+@hello
+def foo():
+    print("i am foo")
+
+foo()
+
+a=hello(foo)
+
+a()
---
+++ py/basic-topic/decorator/s2.py
@@ -0,0 +1,17 @@
+#基本装饰器范例
+
+registry=[]
+def register(fn):
+    registry.append(fn)
+    return fn
+
+@register
+def foo1():
+    print("i am foo1")
+
+@register
+def foo2():
+    print("i am foo2")
+
+for f in registry:
+    f()
