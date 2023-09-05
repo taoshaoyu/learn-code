1206c0436004aafbe73967fa8e0f95da7062777e
taosy <shaoyu.tao@intel.com>
2023-09-05 07:31:39 +0000

    update

--- py/c-extend/s1/setup.py
+++ py/c-extend/s1/setup.py
@@ -1,8 +1,15 @@
 # setup.py
 from distutils.core import setup, Extension
+import sysconfig
+
+extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
+extra_compile_args += ["-std=c++11", "-Wall", "-Wextra"]
+extra_compile_args += ["-g","-O0"]
+
 
 module1 = Extension('demo',
-                    sources = ['hello.c'])
+                    sources = ['hello.c'],
+                    extra_compile_args=extra_compile_args)
 
 setup (name = 'PackageName',
        version = '1.0',
